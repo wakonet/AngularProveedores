af70d824d1b2f9eab30d588394d3590dc276e0c0
rafael.gonzalez <rafael.gonzalez@infotec.mx>
2023-09-27 21:27:43 -0600

    Primer version del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'Gapsi';
+
+  constructor(private router: Router) {}
+  
+
+
+  irProveedores() {
+    // Haz aquí la lógica necesaria para redirigir al otro componente
+    console.log("Entra");
+    this.router.navigate(['/listaproveedores']);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router'; 
 
 import { AppComponent } from './app.component';
+import { ListaproveedoresComponent } from './listaproveedores/listaproveedores.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaproveedoresComponent,
+    PagenotfoundComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule, 
+    AppRoutingModule,
+    HttpClientModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
